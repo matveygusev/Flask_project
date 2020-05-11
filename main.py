2f8550f771b1ea3c406573faae882d0e3c5c3b6b
matveygusev <57760539+matveygusev@users.noreply.github.com>
2020-05-11 03:30:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,139 @@
+from flask import Flask
+from flask_sqlalchemy import sqlalchemy
+from flask_login import LoginManager
+from flask import render_template, redirect, url_for, request, flash
+from flask_login import login_user, login_required, logout_user
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import UserMixin
+from data import db_session
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+from sqlalchemy.orm import Session
+import sqlalchemy.ext.declarative as dec
+import os
+
+SqlAlchemyBase = dec.declarative_base()
+
+__factory = None
+
+app = Flask(__name__)
+manager = LoginManager(app)
+db_session.global_init("db/messanger.sqlite")
+
+
+class Message(SqlAlchemyBase):
+    __tablename__ = 'message'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    text = sqlalchemy.Column(sqlalchemy.String(1024), nullable=False)
+
+    def __init__(self, text, tags):
+        self.text = text.strip()
+        self.tags = [
+            Tag(text=tag.strip()) for tag in tags.split(',')
+        ]
+
+
+class Tag(SqlAlchemyBase):
+    __tablename__ = 'tag'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    text = sqlalchemy.Column(sqlalchemy.String(32), nullable=False)
+    message_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('message.id'), nullable=False)
+
+
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'user'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    login = sqlalchemy.Column(sqlalchemy.String(128), nullable=False, unique=True)
+    password = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+
+
+@manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/main', methods=['GET'])
+@login_required
+def main():
+    return render_template('main.html', messages=Message.query.all())
+
+
+@app.route('/add_message', methods=['POST'])
+@login_required
+def add_message():
+    text = request.form['text']
+    tag = request.form['tag']
+
+    sqlalchemy.session.add(Message(text, tag))
+    sqlalchemy.session.commit()
+
+    return redirect(url_for('main'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if login and password:
+        user = User.query.filter_by(login=login).first()
+
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+
+            next_page = request.args.get('next')
+
+            return redirect(next_page)
+        else:
+            flash('Логин или пароль некорректны')
+    else:
+        flash('Заполните поля логина и пароля')
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    login = request.form.get('login')
+    password = request.form.get('password')
+    password2 = request.form.get('password2')
+
+    if request.method == 'POST':
+        if not (login or password or password2):
+            flash('Заполните все поля!')
+        elif password != password2:
+            flash('Пароли не совпадают!')
+        else:
+            hash_pwd = generate_password_hash(password)
+            new_user = User(login=login, password=hash_pwd)
+            sqlalchemy.session.add(new_user)
+            sqlalchemy.session.commit()
+
+            return redirect(url_for('login_page'))
+
+    return render_template('register.html')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('hello_world'))
+
+
+@app.after_request
+def redirect_to_signin(response):
+    if response.status_code == 401:
+        return redirect(url_for('login_page') + '?next=' + request.url)
+
+    return response
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
